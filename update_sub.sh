1ec1b9fbf9deee5f3b16bfc510aeec5e824e9e86
korwin <levozavr@mail.ru>
2019-07-02 21:15:36 +0300

    add deleting old version

--- update_sub.sh
+++ update_sub.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [[ "$TRAVIS_BRANCH" = "$MASTER_BRANCH" ]]
 then
+    cd levkovalenko.github.io
+    shopt -s extglob
+    rm -rf !(LICENSE|README.md|.gitignore)
+    ls -a
+    cd ..
     mv build/* levkovalenko.github.io
     cd levkovalenko.github.io
     git checkout master
