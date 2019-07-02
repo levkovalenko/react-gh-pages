87946eac24dfbb9017de99bde5bfb35f4c0078fe
korwin <levozavr@mail.ru>
2019-07-02 20:47:39 +0300

    build only on master branch

--- update_sub.sh
+++ update_sub.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-mv build/* levkovalenko.github.io
-cd levkovalenko.github.io
-git checkout master
-git add .
-git status
-git commit -m "AutoRebuild"
-git push -f
-cd ..
+if [[ "$TRAVIS_BRANCH" = "$MASTER_BRANCH" ]]
+then
+    mv build/* levkovalenko.github.io
+    cd levkovalenko.github.io
+    git checkout master
+    git add .
+    git status
+    git commit -m "AutoRebuild"
+    git push -f
+    cd ..
+fi
