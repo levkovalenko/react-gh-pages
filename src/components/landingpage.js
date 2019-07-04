bb6e129fb383d715c95cedf6c1bd28c173bec994
korwin <levozavr@mail.ru>
2019-07-03 22:33:22 -0900

    add a little bit landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,28 @@
 import React, {Component} from "react";
+import {Grid, Cell} from "react-mdl";
 
 
 class Landing extends Component {
     render() {
         return (
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img
+                        src="https://cdn2.iconfinder.com/data/icons/people-3-2/128/Programmer-Avatar-Backend-Developer-Nerd-512.png"
+                        alt="avatar"
+                        className="avatar-img"
+                    />
+                    <div className="banner-text">
+                        <h1>Data Scientist</h1>
+                        <hr/>
+                        <p>
+                            Python | Sklearn | Pandas |  | TensorFlow | Keras
+                        </p>
+                    </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
