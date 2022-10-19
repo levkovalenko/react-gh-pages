b78600135fda19a5c57f3b6f65dbfec63e326981
Lev Kovalenko <levozavr@mail.ru>
2022-10-19 13:28:08 +0300

    Update update_sub.sh

--- update_sub.sh
+++ update_sub.sh
@@ -3,12 +3,11 @@ rm -r levkovalenko.github.io/*[^LICENSE,README.md,.gitignore]
 rm levkovalenko.github.io/*a*
 mv build/* levkovalenko.github.io
 cd levkovalenko.github.io
-git checkout master
 git add .
 git status
 git config --global user.email "levozavr@mail.ru"
 git config --global user.name "Lev Kovalenko"
 git commit -m "AutoRebuild"
-git push -f
+git push -f origin HEAD:master
 cd ..
 
